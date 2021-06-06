d10b16baf0aace59d0e6c765d1f6496ec362f705
paigexx <76425112+paigexx@users.noreply.github.com>
2021-06-06 14:10:32 -0500

    icons added

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var right_col = document.querySelector("#right_col")
 var left_col = document.querySelector("#left_col")
 var units = ""
 var no_result_p = document.querySelector("#not_activated")
+var city_header = document.querySelector("#city_header")
+var icon = ""
+
 
 
 function get_q(element){
@@ -13,7 +16,6 @@ function get_q(element){
 
 function define_units(element){
     units = element.value
-    alert("You choose: " + units)
     console.log (units)
 
 }
@@ -22,15 +24,20 @@ function remove(){
     no_result_p.remove()
 }
 
+function city_header_section(data){
+    var icon = data['weather'][0]['icon']
+    var title_img = `<h1>${current_q}</h1>
+                    <img src="http://openweathermap.org/img/w/${icon}.png" alt="icon">`
+    return icon, title_img
+}
+
 
 function left_col_content(data){
-    var res_left=    `<div class="weather_content_container">
+    var res_left=   `<div class="weather_content_container">
                         <p>City: ${data.name}</p> 
                         <p>Main: ${data['weather'][0]['main']}</p>
-                        <p>Current Temperature: ${data.main.temp}</p>
-                        
-                
-                </div>`
+                        <p>Current Temp: ${data.main.temp}</p>        
+                    </div>`
     return res_left
 
 }
@@ -45,11 +52,14 @@ function right_col_content(data){
 }
 
 async function search(){
-    var response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + current_q + "&units=" + units + "&appid=10be953ca29615b40b69f861711e0b54")
+
+    var response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + current_q + "&units=" + units + "&appid=10be953ca29615b40b69f861711e0b54") 
     var weather_data = await response.json()
     console.log(weather_data)
+    city_header.innerHTML = city_header_section(weather_data)
     right_col.innerHTML = right_col_content(weather_data)
     left_col.innerHTML = left_col_content(weather_data)
+    
 
 }
 
